22b2bff5196da6a41656c315121d2cc104da2a9f
Matheus Lessa <mtheuslessa96@gmail.com>
2022-09-13 17:36:39 -0300

    Initial commit

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChangeColor : MonoBehaviour{
+    private Renderer _cube;
+    private Color _newCubeColor;
+    private float _random1, _random2, _random3;
+
+    private void Awake(){
+        _cube = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        _random1 = Random.Range(0f, 1f);
+        _random2 = Random.Range(0f, 1f);
+        _random3 = Random.Range(0f, 1f);
+
+        _newCubeColor = new Color(_random1, _random2, _random3);
+        
+        _cube.material.SetColor("_Color", _newCubeColor);
+    }
+}
