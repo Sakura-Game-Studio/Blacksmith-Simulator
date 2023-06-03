c3fa74829db935036c2a1ca74cf5b9fbfa0769c1
Matheus Lessa <mtheuslessa96@gmail.com>
2023-06-03 16:08:41 -0300

    Estante

---
+++ Assets/Assets/Scripts/Castor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castor : MonoBehaviour{
+    [SerializeField] private BoxCollider colisorCastor;
+
+    [SerializeField] private List<ItemSO> itensGaveta;
+    
+}
